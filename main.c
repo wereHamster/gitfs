99aaa5480225617d2ac2271b272badc196f8fbf1
Tomas Carnecky <tom@dbservice.com>
2009-07-22 01:28:56 +0200

    *hack* *hack* *hack*

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-/*
-    FUSE: Filesystem in Userspace
-    Copyright (C) 2001-2005  Miklos Szeredi <miklos@szeredi.hu>
 
-    This program can be distributed under the terms of the GNU GPL.
-    See the file COPYING.
-*/
+#include <assert.h>
 
 #define FUSE_USE_VERSION 27
-
 #include <fuse.h>
 
 #include <cache.h>
@@ -15,44 +9,50 @@
 #include <tree.h>
 #include <commit.h>
 #include <tag.h>
+#include <diff.h>
+#include <revision.h>
 
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *__gitfs_str = "__Gitfs World!\n";
-static const char *__gitfs_path = "/__gitfs";
+struct __gitfs_object {
+	unsigned char rev[20];
+	struct object *obj;
+	unsigned mode;
+};
 
-struct object *__object(const char *path)
+static const char *parse(const char *path, unsigned char sha1[20])
 {
-	char *ref = xstrdup("HEAD");
-	char *sub = xstrdup(path + 1);
+	const char *ref = "HEAD", *end = ref + 4;
 
-	if (strncmp(path, "/.refs/", 7) == 0) {
-		const char *end = strchr(path + 7, '/');
-		if (end == NULL) {
-			ref = xstrdup(path + 7);
-			sub = xstrdup("");
-		} else {
-			ref = xstrndup(path + 7, end - path - 7);
-			sub = xstrdup(end + 1);
+	if (strncmp(path, "/.refs", 6) == 0) {
+		if (path[6] == '\0')
+			return NULL;
+
+		if (path[6] == '/') {
+			path += 7;
+			ref = path;
+
+			end = strchr(ref, '/');
+			if (end == NULL)
+				end = path + strlen(path);
+
+			path = end[0] == '/' ? end + 1 : end;
 		}
+	} else {
+		path += 1;
 	}
 
-	char tmp[1000];
-	sprintf(tmp, "%s:%s", ref, sub);
-
-	free(ref);
-	free(sub);
-
-	unsigned char sha1[20];
-	int ret = get_sha1(tmp, sha1);
-	printf("path %s resolved to %s\n", path, tmp);
-
-	if (ret)
+	if (get_sha1_1(ref, end - ref, sha1))
 		return NULL;
+	
+	return path;
+}
 
+struct object *gitobj(unsigned char sha1[20])
+{
 	struct object *obj = parse_object(sha1);
         do {
                 if (!obj)
@@ -71,49 +71,168 @@ struct object *__object(const char *path)
         } while (1);
 }
 
+struct __gitfs_object *__gitfs(const char *path)
+{
+	unsigned char sha1[20];
+	const char *dir = parse(path, sha1);
+	if (!dir)
+		return NULL;
+
+	struct object *obj = gitobj(sha1);
+	if (!obj)
+		return NULL;
+
+	unsigned mode = 0644;
+	if (obj->type == OBJ_TREE) {
+		unsigned char blob[20];
+		if (get_tree_entry(sha1, dir, blob, &mode))
+			return NULL;
+
+		obj = gitobj(blob);
+	}
+
+	struct __gitfs_object *gfsobj = malloc(sizeof(struct __gitfs_object));
+	if (gfsobj) {
+		hashcpy(gfsobj->rev, sha1);
+		gfsobj->obj = obj;
+		gfsobj->mode = mode;
+	}
+
+	return gfsobj;
+}
+
 static int __gitfs_getattr(const char *path, struct stat *stbuf)
 {
 	int res = 0;
 
 	memset(stbuf, 0, sizeof(struct stat));
 
-	struct object *obj = __object(path);
-	if (obj == NULL) {
-		if (strncmp(path, "/.refs", 6))
-			return -ENOENT;
-
-		if (strcmp(path, "/.refs") == 0) {
-			stbuf->st_mode = S_IFDIR | 0755;
-			stbuf->st_nlink = 2;
-		} else {
-			const char *ref = path + 7;
-
-			unsigned char sha1[20];
-			if (get_sha1(ref, sha1))
-				return -ENOENT;
-
-			stbuf->st_mode = S_IFDIR | 0755;
-			stbuf->st_nlink = 2;
-		}
+	if (strcmp(path, "/.refs") == 0) {
+		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
 
 		return 0;
 	}
 
-	printf("object type: %d\n", obj->type);
+	struct __gitfs_object *obj = __gitfs(path);
+	if (obj == NULL)
+		return -ENOENT;
 
-	if (obj->type == OBJ_TREE) {
+	if (obj->obj->type == OBJ_TREE) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else if (obj->type == OBJ_BLOB) {
-		stbuf->st_mode = S_IFREG | 0444;
+	} else if (obj->obj->type == OBJ_BLOB) {
+		stbuf->st_mode = S_IFREG | obj->mode;
 		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(__gitfs_str);
+
+		sha1_object_info(obj->obj->sha1, &stbuf->st_size);
 	} else {
 		res = -ENOENT;
 	}
 
+	free(obj);
+
 	return res;
 }
+
+static int __gitfs_readlink(const char *path, char *buf, size_t size)
+{
+	unsigned long len;
+
+	struct __gitfs_object *obj = __gitfs(path);
+	if (!obj)
+		return -ENOENT;
+	
+	enum object_type type;
+	void *data = read_sha1_file(obj->obj->sha1, &type, &len);
+
+	assert(type == obj->obj->type);
+	free(obj);
+
+	if (len > size)
+		len = size;
+
+	memcpy(buf, data, len);
+	free(data);
+
+	return 0;
+}
+
+static int __gitfs_open(const char *path, struct fuse_file_info *fi)
+{
+	struct __gitfs_object *obj = __gitfs(path);
+	if (!obj)
+		return -ENOENT;
+
+	if (obj->obj->type == OBJ_TREE)
+		return -EISDIR;
+
+	assert(obj->obj->type == OBJ_BLOB);
+
+	if ((fi->flags & 3) != O_RDONLY)
+		return -EACCES;
+
+	fi->fh = (uint64_t) obj;
+
+	return 0;
+}
+
+static int __gitfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+	unsigned long len;
+
+	struct __gitfs_object *obj = (struct __gitfs_object *) fi->fh;
+	assert(obj);
+
+	if (obj->obj->type != OBJ_BLOB)
+		return -ENOENT;
+	
+	enum object_type type;
+	void *data = read_sha1_file(obj->obj->sha1, &type, &len);
+
+	assert(type == obj->obj->type);
+
+	if (offset < len) {
+		if (offset + size > len)
+			size = len - offset;
+		memcpy(buf, data + offset, size);
+	} else {
+		size = 0;
+	}
+
+	free(data);
+
+	return size;
+}
+
+static int __gitfs_release(const char *path, struct fuse_file_info *fi)
+{
+	struct __gitfs_object *obj = (struct __gitfs_object *) fi->fh;
+	assert(obj);
+
+	free(obj);
+
+	return 0;
+}
+
+static int __gitfs_getxattr(const char *path, const char *name, char *value, size_t size)
+{
+	struct __gitfs_object *obj = __gitfs(path);
+	if (!obj)
+		return -ENOENT;
+
+	if (strcmp(name, "__gitfs.hash") == 0) {
+		strncpy(value, sha1_to_hex(obj->obj->sha1), size);
+	}
+
+	return 0;
+}
+
+static int __gitfs_opendir(const char *path, struct fuse_file_info *fi)
+{
+	return -ENOENT;
+}
+
 
 struct __gitfs_readdir_ctx {
 	void *buf;
@@ -134,13 +253,12 @@ static int show_ref(const char *refname, const unsigned char *sha1, int flag, vo
 {
 	struct __gitfs_readdir_ctx *ctx = context;
 
-        (*ctx->filler) (ctx->buf, strrchr(refname, '/') + 1, NULL, 0);
+        (*ctx->filler) (ctx->buf, refname, NULL, 0);
 
         return 0;
 }
 
-static int __gitfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi)
+static int __gitfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
 	struct __gitfs_readdir_ctx ctx = { buf, filler };
 
@@ -148,68 +266,58 @@ static int __gitfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		filler(buf, ".", NULL, 0);
 		filler(buf, "..", NULL, 0);
 
-		for_each_ref(show_ref, &ctx);
+		for_each_tag_ref(show_ref, &ctx);
 		return 0;
 	}
 
-	struct object *obj = __object(path);
-	if (!obj || obj->type != OBJ_TREE)
+	struct __gitfs_object *obj = __gitfs(path);
+	if (!obj || obj->obj->type != OBJ_TREE)
 		return -ENOENT;
 
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
-        struct tree *tree = (struct tree *) obj;
+        struct tree *tree = (struct tree *) obj->obj;
 
         read_tree_recursive(tree, "", 0, 0, NULL, show_tree, &ctx);
+
+	free(obj);
 	
-//	filler(buf, __gitfs_path + 1, NULL, 0);
-
 	return 0;
 }
 
-static int __gitfs_open(const char *path, struct fuse_file_info *fi)
+static int __gitfs_releasedir(const char *path, struct fuse_file_info *fi)
 {
-	struct object *obj = __object(path);
-	if (!obj || obj->type != OBJ_BLOB)
-		return -ENOENT;
-
-	if((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
-
-	return 0;
+	return -ENOENT;
 }
 
-static int __gitfs_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
+static int __gitfs_access(const char *path, int mode)
 {
-	size_t len;
-
-	struct object *obj = __object(path);
-	if (!obj || obj->type != OBJ_BLOB)
-		return -ENOENT;
-
-	len = strlen(__gitfs_str);
-	if (offset < len) {
-		if (offset + size > len)
-			size = len - offset;
-		memcpy(buf, __gitfs_str + offset, size);
-	} else
-		size = 0;
-
-	return size;
+	return -ENOENT;
 }
 
-static struct fuse_operations ops = {
-	.getattr = __gitfs_getattr,
-	.readdir = __gitfs_readdir,
-	.open = __gitfs_open,
-	.read = __gitfs_read,
+static int __gitfs_fgetattr(const char *path, struct stat *buf, struct fuse_file_info *fi)
+{
+	return -ENOENT;
+}
+
+static struct fuse_operations __gitfs_ops = {
+	.getattr    = __gitfs_getattr,
+	.readlink   = __gitfs_readlink,
+	.open       = __gitfs_open,
+	.read       = __gitfs_read,
+	.release    = __gitfs_release,
+//	.getxattr   = __gitfs_getxattr,
+//	.opendir    = __gitfs_opendir,
+	.readdir    = __gitfs_readdir,
+//	.releasedir = __gitfs_releasedir,
+//	.access     = __gitfs_access,
+//	.fgetattr   = __gitfs_fgetattr,
 };
 
 int main(int argc, char *argv[])
 {
 	git_config(git_default_config, NULL);
 
-	return fuse_main(argc, argv, &ops, NULL);
+	return fuse_main(argc, argv, &__gitfs_ops, NULL);
 }
